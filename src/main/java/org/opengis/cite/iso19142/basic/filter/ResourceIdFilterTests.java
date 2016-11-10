5c2d7c4b365c4e6b4288f353415f0fe795752abc
R. Martell <rjmartell@computer.org>
2016-11-10 11:56:35 -0800

    Fix #38: Use GET method only

--- src/main/java/org/opengis/cite/iso19142/basic/filter/ResourceIdFilterTests.java
+++ src/main/java/org/opengis/cite/iso19142/basic/filter/ResourceIdFilterTests.java
@@ -39,94 +39,81 @@ import com.sun.jersey.api.client.ClientResponse;
  */
 public class ResourceIdFilterTests extends QueryFilterFixture {
 
-	/**
-	 * [{@code Test}] Submits a GetFeature request containing a ResourceId
-	 * predicate with two resource identifiers. The response entity must include
-	 * only instances of the requested type with matching gml:id attribute
-	 * values.
-	 * 
-	 * @param binding
-	 *            The ProtocolBinding to use for this request.
-	 * @param featureType
-	 *            A QName representing the qualified name of some feature type.
-	 */
-	@Test(description = "See ISO 19142: 7.2.2; ISO 19143: 7.11", dataProvider = "protocol-featureType")
-	public void twoValidFeatureIdentifiers(ProtocolBinding binding,
-			QName featureType) {
-		WFSMessage.appendSimpleQuery(this.reqEntity, featureType);
-		Set<String> idSet = this.dataSampler.selectRandomFeatureIdentifiers(
-				featureType, 2);
-		WFSMessage.addResourceIdPredicate(this.reqEntity, idSet);
-		ClientResponse rsp = wfsClient.submitRequest(reqEntity, binding);
-		this.rspEntity = extractBodyAsDocument(rsp);
-		Assert.assertEquals(rsp.getStatus(),
-				ClientResponse.Status.OK.getStatusCode(),
-				ErrorMessage.get(ErrorMessageKeys.UNEXPECTED_STATUS));
-		ETSAssert.assertDescendantElementCount(this.rspEntity, featureType,
-				idSet.size());
-	}
+    /**
+     * [{@code Test}] Submits a GetFeature request containing a ResourceId
+     * predicate with two resource identifiers. The response entity must include
+     * only instances of the requested type with matching gml:id attribute
+     * values.
+     * 
+     * @param binding
+     *            The ProtocolBinding to use for this request.
+     * @param featureType
+     *            A QName representing the qualified name of some feature type.
+     */
+    @Test(description = "See ISO 19142: 7.2.2; ISO 19143: 7.11", dataProvider = "protocol-featureType")
+    public void twoValidFeatureIdentifiers(ProtocolBinding binding, QName featureType) {
+        WFSMessage.appendSimpleQuery(this.reqEntity, featureType);
+        Set<String> idSet = this.dataSampler.selectRandomFeatureIdentifiers(featureType, 2);
+        WFSMessage.addResourceIdPredicate(this.reqEntity, idSet);
+        ClientResponse rsp = wfsClient.submitRequest(reqEntity, binding);
+        this.rspEntity = extractBodyAsDocument(rsp);
+        Assert.assertEquals(rsp.getStatus(), ClientResponse.Status.OK.getStatusCode(),
+                ErrorMessage.get(ErrorMessageKeys.UNEXPECTED_STATUS));
+        ETSAssert.assertDescendantElementCount(this.rspEntity, featureType, idSet.size());
+    }
 
-	/**
-	 * [{@code Test}] Submits a GetFeature request containing a ResourceId
-	 * predicate with an unknown feature identifier. The response entity is
-	 * expected to be a wfs:FeatureCollection that has no matching feature
-	 * member.
-	 * 
-	 * @param binding
-	 *            The ProtocolBinding to use for this request (e.g. GET, POST).
-	 * @param featureType
-	 *            A QName representing the qualified name of some supported
-	 *            feature type.
-	 */
-	@Test(description = "See ISO 19142: 7.2.2, Table 8", dataProvider = "protocol-featureType")
-	public void unknownFeatureIdentifier(ProtocolBinding binding,
-			QName featureType) {
-		WFSMessage.appendSimpleQuery(this.reqEntity, featureType);
-		Set<String> idSet = new HashSet<String>();
-		idSet.add("test-" + UUID.randomUUID());
-		WFSMessage.addResourceIdPredicate(this.reqEntity, idSet);
-		ClientResponse rsp = wfsClient.submitRequest(reqEntity, binding);
-		this.rspEntity = extractBodyAsDocument(rsp);
-		Assert.assertEquals(rsp.getStatus(),
-				ClientResponse.Status.OK.getStatusCode(),
-				ErrorMessage.get(ErrorMessageKeys.UNEXPECTED_STATUS));
-		ETSAssert.assertQualifiedName(this.rspEntity.getDocumentElement(),
-				new QName(Namespaces.WFS, WFS2.FEATURE_COLLECTION));
-		ETSAssert.assertFeatureCount(this.rspEntity, featureType, 0);
-	}
+    /**
+     * [{@code Test}] Submits a GetFeature request containing a ResourceId
+     * predicate with an unknown feature identifier. The response entity is
+     * expected to be a wfs:FeatureCollection that has no matching feature
+     * member.
+     * 
+     * @param binding
+     *            The ProtocolBinding to use for this request (e.g. GET, POST).
+     * @param featureType
+     *            A QName representing the qualified name of some supported
+     *            feature type.
+     */
+    @Test(description = "See ISO 19142: 7.2.2, Table 8", dataProvider = "protocol-featureType")
+    public void unknownFeatureIdentifier(ProtocolBinding binding, QName featureType) {
+        WFSMessage.appendSimpleQuery(this.reqEntity, featureType);
+        Set<String> idSet = new HashSet<String>();
+        idSet.add("test-" + UUID.randomUUID());
+        WFSMessage.addResourceIdPredicate(this.reqEntity, idSet);
+        ClientResponse rsp = wfsClient.submitRequest(reqEntity, binding);
+        this.rspEntity = extractBodyAsDocument(rsp);
+        Assert.assertEquals(rsp.getStatus(), ClientResponse.Status.OK.getStatusCode(),
+                ErrorMessage.get(ErrorMessageKeys.UNEXPECTED_STATUS));
+        ETSAssert.assertQualifiedName(this.rspEntity.getDocumentElement(),
+                new QName(Namespaces.WFS, WFS2.FEATURE_COLLECTION));
+        ETSAssert.assertFeatureCount(this.rspEntity, featureType, 0);
+    }
 
-	/**
-	 * [{@code Test}] If a feature instance identified by the RESOURCEID
-	 * parameter is not of the type specified by the TYPENAMES parameter, the
-	 * server shall raise an InvalidParameterValue exception where the "locator"
-	 * attribute value shall be set to "RESOURCEID".
-	 * 
-	 * @param binding
-	 *            The ProtocolBinding to use for this request.
-	 * @param featureType
-	 *            A QName representing the qualified name of some feature type.
-	 * 
-	 * @see "OGC 09-025r1, cl. 7.9.2.4.1: typeNames parameter"
-	 */
-	@Test(description = "See ISO 19142: 7.9.2.4.1", dataProvider = "protocol-featureType")
-	public void inconsistentFeatureIdentifierAndType(ProtocolBinding binding,
-			QName featureType) {
-		WFSMessage.appendSimpleQuery(this.reqEntity, featureType);
-		Set<String> idSet = new HashSet<String>();
-		String featureId = this.dataSampler.getFeatureId(featureType, false);
-		if (null == featureId) {
-			throw new SkipException(
-					"Unable to find id of feature instance that is NOT of type "
-							+ featureType);
-		}
-		idSet.add(featureId);
-		WFSMessage.addResourceIdPredicate(this.reqEntity, idSet);
-		ClientResponse rsp = wfsClient.submitRequest(reqEntity, binding);
-		this.rspEntity = extractBodyAsDocument(rsp);
-		Assert.assertEquals(rsp.getStatus(),
-				ClientResponse.Status.BAD_REQUEST.getStatusCode(),
-				ErrorMessage.get(ErrorMessageKeys.UNEXPECTED_STATUS));
-		ETSAssert.assertExceptionReport(this.rspEntity,
-				"InvalidParameterValue", "RESOURCEID");
-	}
+    /**
+     * [{@code Test}] If a feature instance identified by the RESOURCEID
+     * parameter is not of the type specified by the TYPENAMES parameter, the
+     * server shall raise an InvalidParameterValue exception where the "locator"
+     * attribute value shall be set to "RESOURCEID".
+     * 
+     * @param featureType
+     *            A QName representing the qualified name of some feature type.
+     * 
+     * @see "OGC 09-025r1, cl. 7.9.2.4.1: typeNames parameter"
+     */
+    @Test(description = "See ISO 19142: 7.9.2.4.1", dataProvider = "instantiated-feature-types")
+    public void inconsistentFeatureIdentifierAndType(QName featureType) {
+        WFSMessage.appendSimpleQuery(this.reqEntity, featureType);
+        Set<String> idSet = new HashSet<String>();
+        String featureId = this.dataSampler.getFeatureId(featureType, false);
+        if (null == featureId) {
+            throw new SkipException("Unable to find id of feature instance that is NOT of type " + featureType);
+        }
+        idSet.add(featureId);
+        WFSMessage.addResourceIdPredicate(this.reqEntity, idSet);
+        ClientResponse rsp = wfsClient.submitRequest(reqEntity, ProtocolBinding.GET);
+        this.rspEntity = extractBodyAsDocument(rsp);
+        Assert.assertEquals(rsp.getStatus(), ClientResponse.Status.BAD_REQUEST.getStatusCode(),
+                ErrorMessage.get(ErrorMessageKeys.UNEXPECTED_STATUS));
+        ETSAssert.assertExceptionReport(this.rspEntity, "InvalidParameterValue", "RESOURCEID");
+    }
 }
