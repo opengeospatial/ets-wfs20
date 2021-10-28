ea97cba984f5dad9cc9c9f4291339ff09da4a589
Benjamin Proß <b.pross@52north.org>
2021-10-28 10:33:13 +0200

    Add test

--- src/test/java/org/opengis/cite/iso19142/basic/filter/VerifyResourceId.java
+++ src/test/java/org/opengis/cite/iso19142/basic/filter/VerifyResourceId.java
@@ -3,10 +3,22 @@ package org.opengis.cite.iso19142.basic.filter;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.Test;
+import org.opengis.cite.iso19142.ETSAssert;
 import org.opengis.cite.iso19142.Namespaces;
+import org.opengis.cite.iso19142.WFS2;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 public class VerifyResourceId {
 
@@ -34,5 +46,23 @@ public class VerifyResourceId {
         Element idElem = id.toElement();
         assertTrue(idElem.getNamespaceURI().equals(Namespaces.FES));
         assertTrue("Found @previousRid", idElem.getAttribute("previousRid").isEmpty());
+    }
+
+    @Test
+    public void assertElementCount() {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder docBuilder = null;
+        try {
+			docBuilder = dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			fail("Could not set up DocumentBuilder.");
+		}
+        try {
+			Document doc = docBuilder.parse(getClass().getClassLoader().getResourceAsStream("GetFeature/FeatureCollection-River.xml"));
+			ETSAssert.assertDescendantElementCount(doc, new QName(Namespaces.WFS, WFS2.MEMBER), 4);
+		} catch (SAXException | IOException e) {
+			fail("Could not parse feature collection.");
+		}
     }
 }
